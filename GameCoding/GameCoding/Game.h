fad49977d8fd640ea99ec41ddc1401763541eeb1
cubedice <cubedice725@gmail.com>
2024-03-21 21:05:00 +0900

    DirectX 11 초기화(2024.03.21)
    진행상황
    배경 색상 바꾸기
    
    특이사항
    없음

--- GameCoding/GameCoding/Game.h
+++ GameCoding/GameCoding/Game.h
@@ -9,7 +9,27 @@ public:
 	void Update();
 	void Render();
 private:
+	void RenderBegin();
+	void RenderEnd();
+private:
+	void CreateDeviceAndSqapChain();
+	void CreateRenderTargetView();
+	void SetViewport();
+private:
 	HWND _hwnd;
 	uint32 _width = 0;
 	uint32 _height = 0;
+
+private:
+	//DX
+	ComPtr<ID3D11Device> _device = nullptr;
+	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
+
+	//RTV
+	ComPtr<ID3D11RenderTargetView> _renderTargetView;
+
+	//Misc
+	D3D11_VIEWPORT _viewport = { 0 };
+	float _clearColor[4] = { 0.5f, 0.5f, 0.5f, 0.5f };
 };
